b15ce9ceadd10e3afe6a1f7212a8ab94b622ce76
Dmitry Komanov <dmitryk@wix.com>
2017-01-28 21:14:38 +0200

    Add hack for Intellij to recognize all dependencies

---
+++ src/com/komanov/ideahack/Main.java
@@ -0,0 +1,7 @@
+package com.komanov.ideahack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This binary is a way for IDEA plugin to properly recognize all dependencies.");
+    }
+}
