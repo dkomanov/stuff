ef08a330a09cec2bca014fdfd7ddb274c2ea2e8a
Dmitry Komanov <dmitryk@wix.com>
2017-11-14 23:54:01 +0200

    Remove ideahack, apparently bazel plugin supports scala

--- src/com/komanov/ideahack/Main.java
+++
@@ -1,7 +0,0 @@
-package com.komanov.ideahack;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("This binary is a way for IDEA plugin to properly recognize all dependencies.");
-    }
-}
