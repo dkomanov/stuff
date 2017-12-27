7d95f59a352cb7444ec263f4cc7e97235ce3e295
Dmitry Komanov <dmitryk@wix.com>
2017-12-27 22:26:16 +0200

    Vars for destination paths, git add

--- scripts/read-lines.sh
+++ scripts/read-lines.sh
@@ -12,9 +12,14 @@ then
   exit 1
 fi
 
+JSON_PATH="${DATA_DIR}/jmh_${TS}_${DRIVE_TYPE}.json"
+LOG_PATH="${DATA_DIR}/jmh_${TS}_${DRIVE_TYPE}.log"
+
 bazel run //${TARGET_DIR}/${TARGET_NAME} -- -rf json -rff jmh.json ReadLinesBenchmark | tee jmh.log
-mv ./bazel-bin/${TARGET_DIR}/${TARGET_NAME}/${TARGET_NAME}.runfiles/stuff/jmh.json ${DATA_DIR}/jmh_${TS}_${DRIVE_TYPE}.json
-mv ./jmh.log ${DATA_DIR}/jmh_${TS}_${DRIVE_TYPE}.log
+mv ./bazel-bin/${TARGET_DIR}/${TARGET_NAME}/${TARGET_NAME}.runfiles/stuff/jmh.json ${JSON_PATH}
+mv ./jmh.log ${LOG_PATH}
+
+git add ${JSON_PATH} ${LOG_PATH}
 
 echo ${TS}
 echo "Don't forget to change JS, commit, push and deploy!"
