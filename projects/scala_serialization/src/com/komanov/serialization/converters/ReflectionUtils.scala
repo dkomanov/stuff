132eb28dab4cd2c39647f1042f9b2be00bfd7881
Dmitry Komanov <dmitryk@wix.com>
2018-04-03 23:44:18 +0300

    Make ReflectionUtils package-private

--- projects/scala_serialization/src/com/komanov/serialization/converters/ReflectionUtils.scala
+++ projects/scala_serialization/src/com/komanov/serialization/converters/ReflectionUtils.scala
@@ -1,6 +1,6 @@
 package com.komanov.serialization.converters
 
-object ReflectionUtils {
+private[converters] object ReflectionUtils {
 
   def getCompanionObject(clazz: Class[_]): Any = {
     import scala.reflect.runtime.{currentMirror => cm}
