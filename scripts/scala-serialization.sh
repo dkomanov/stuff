1002ccf80a8691d736c884e8205d4020f93fee3f
Dmitry Komanov <dmitryk@wix.com>
2018-04-08 21:10:38 +0300

    fix paths

--- scripts/scala-serialization.sh
+++ scripts/scala-serialization.sh
@@ -5,13 +5,13 @@ DIR=`pwd $DIR`
 TS=`date --iso-8601`
 TARGET=src/com/komanov/serialization/jmh
 TARGET_NAME=`basename $TARGET`
-DATA_DIR=${DIR}/site/public/data/scala-serialization
+DATA_DIR=${DIR}/site/public/data/charts/scala-serialization
 
-if test ! -d /the/dir; then echo "no data dir, maybe you forgot to link site?"; exit 1; fi
+if test ! -d ${DATA_DIR}; then echo "no data dir, maybe you forgot to link site?"; exit 1; fi
 
 cd ${DIR}/projects/scala_serialization
 bazel run -- //${TARGET} -rf json -rff jmh.json | tee jmh.log
-mv ./bazel-bin/${TARGET}/${TARGET_NAME}.runfiles/stuff/jmh.json ${DATA_DIR}/jmh_${TS}.json
+mv ./bazel-bin/${TARGET}/${TARGET_NAME}.runfiles/scala_serialization/jmh.json ${DATA_DIR}/jmh_${TS}.json
 mv ./jmh.log ${DATA_DIR}/jmh_${TS}.log
 cd ${DIR}
 
