a1691c89323920420a79157cc5b8b22d4515c180
Dmitry Komanov <dmitryk@wix.com>
2019-08-03 08:58:19 +0300

    Output logs on failure

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ build() {
   bazel $BAZEL_OPTS build --curses=no $TARGET
   echo -en 'travis_fold:end:build\\r'
   echo 'running tests...' && echo -en 'travis_fold:start:test\\r'
-  bazel $BAZEL_OPTS test --curses=no $TARGET
+  bazel $BAZEL_OPTS test --test_output=errors --curses=no $TARGET
   echo -en 'travis_fold:end:test\\r'
 
   echo 'complete '$1 && echo -en 'travis_fold:end:'$1'\\r'
