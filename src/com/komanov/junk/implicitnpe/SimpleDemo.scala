269777e76dfbc982c601a1c54bda7411657221e1
Dmitry Komanov <dmitryk@wix.com>
2020-05-20 21:23:57 +0300

    Add strange test case for implicit
    https://stackoverflow.com/questions/61917884/nullpointerexception-on-implicit-resolution

---
+++ src/com/komanov/junk/implicitnpe/SimpleDemo.scala
@@ -0,0 +1,26 @@
+package com.komanov.junk.implicitnpe
+
+import com.komanov.junk.implicitnpe.anotherpackage.MyContext
+import com.komanov.junk.implicitnpe.anotherpackage.builders.aMyContext
+
+// https://stackoverflow.com/questions/61917884/nullpointerexception-on-implicit-resolution
+case class Context(id: String)
+object Context {
+  implicit def `ContextHolder to Context`(implicit holder: ContextHolder): Context = holder.context
+}
+trait ContextHolder {
+  def context: Context
+}
+
+object anotherpackage {
+  case class MyContext(name: String, context: Context) extends ContextHolder
+
+  object builders {
+    def aMyContext(name: String)(implicit context: Context = Context("test")): MyContext =
+      MyContext(name, context)
+  }
+}
+
+object SimpleDemo extends App {
+  implicit val myContext: MyContext = aMyContext("name")
+}
