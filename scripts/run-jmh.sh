57a533d088b4855c3d7d54a951e2cf069a9cb410
Dmitry Komanov <dmitryk@wix.com>
2022-10-07 17:14:04 +0300

    Align script with project layout

--- scripts/run-jmh.sh
+++ scripts/run-jmh.sh
@@ -12,18 +12,19 @@ then
   exit 1
 fi
 
-SCRIPT=`realpath $0`
-SCRIPT_DIR=`dirname $SCRIPT`
-ROOT_DIR=`realpath $SCRIPT_DIR/..`
-DATA_DIR=$ROOT_DIR/site/static/data/charts/$NAME
+SCRIPT=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+ROOT_DIR=$(realpath "$SCRIPT_DIR"/..)
+SITE_DIR=$(realpath "$ROOT_DIR"/../dkomanov.github.io)
+DATA_DIR=$SITE_DIR/static/data/charts/$NAME
 
-mkdir -p $DATA_DIR
-cd $ROOT_DIR
+mkdir -p "$DATA_DIR"
+cd "$ROOT_DIR"
 
 LABEL="${TARGET}_deploy.jar"
-bazel build $LABEL
-JAR_FILE=`bazel cquery --output starlark --starlark:expr '"\n".join([f.path for f in target.files.to_list()])' $LABEL`
+bazel build "$LABEL"
+JAR_FILE=$(bazel cquery --output starlark --starlark:expr '"\n".join([f.path for f in target.files.to_list()])' "$LABEL")
 
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar $JAR_FILE -jvmArgsAppend "$JDK8_ARGS_APPEND" -rf json -rff $DATA_DIR/jdk8.json  | stdbuf -oL sed "s#${ROOT_DIR}/site##g" | tee $DATA_DIR/jdk8.log.txt
-/usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar $JAR_FILE -jvmArgsAppend "$JDK11_ARGS_APPEND" -rf json -rff $DATA_DIR/jdk11.json | stdbuf -oL sed "s#${ROOT_DIR}/site##g" | tee $DATA_DIR/jdk11.log.txt
-/usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar $JAR_FILE -jvmArgsAppend "$JDK17_ARGS_APPEND" -rf json -rff $DATA_DIR/jdk17.json | stdbuf -oL sed "s#${ROOT_DIR}/site##g" | tee $DATA_DIR/jdk17.log.txt
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar "$JAR_FILE" -jvmArgsAppend "$JDK8_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk8.json  | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk8.log.txt
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK11_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk11.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk11.log.txt
+/usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK17_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk17.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk17.log.txt
