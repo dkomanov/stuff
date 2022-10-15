780e3ed324729a5a2f9c82bbfe2156863deb1306
Dmitry Komanov <dmitryk@wix.com>
2022-10-15 04:09:32 +0300

    Extract BenchmarkBase

---
+++ src/com/komanov/jmh/BenchmarkBase.scala
@@ -0,0 +1,14 @@
+package com.komanov.jmh
+
+import org.openjdk.jmh.annotations._
+
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(value = 1, jvmArgs = Array("-Xmx2G"))
+@Threads(2)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+abstract class BenchmarkBase
