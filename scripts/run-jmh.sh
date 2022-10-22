50eeb3395ee140e4589fcc580e5b8cb2ec8edd39
Dmitry Komanov <dmitryk@wix.com>
2022-10-22 12:06:07 +0300

    Add common argsAppend

--- scripts/run-jmh.sh
+++ scripts/run-jmh.sh
@@ -25,6 +25,6 @@ LABEL="${TARGET}_deploy.jar"
 bazel build "$LABEL"
 JAR_FILE=$(bazel cquery --output starlark --starlark:expr '"\n".join([f.path for f in target.files.to_list()])' "$LABEL")
 
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar "$JAR_FILE" -jvmArgsAppend "$JDK8_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk8.json  | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk8.log.txt
-/usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK11_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk11.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk11.log.txt
-/usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK17_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk17.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk17.log.txt
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND  $JDK8_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk8.json  | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk8.log.txt
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK11_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk11.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk11.log.txt
+/usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK17_ARGS_APPEND" -rf json -rff "$DATA_DIR"/jdk17.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk17.log.txt
