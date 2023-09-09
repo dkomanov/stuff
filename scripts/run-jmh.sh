49ecb5d5d71f7d43603248d6cb7338adca6e1988
Dmitry Komanov <dmitryk@wix.com>
2023-09-09 17:09:29 +0300

    Add ability to disable certain JDK

--- scripts/run-jmh.sh
+++ scripts/run-jmh.sh
@@ -25,6 +25,6 @@ LABEL="${TARGET}_deploy.jar"
 bazel build "$LABEL"
 JAR_FILE=$(bazel cquery --output starlark --starlark:expr '"\n".join([f.path for f in target.files.to_list()])' "$LABEL")
 
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND  $JDK8_ARGS_APPEND" $JDK_JMH_EXTRA  $JDK8_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk8.json  | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk8.log.txt
-/usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK11_ARGS_APPEND" $JDK_JMH_EXTRA $JDK11_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk11.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk11.log.txt
-/usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK17_ARGS_APPEND" $JDK_JMH_EXTRA $JDK17_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk17.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk17.log.txt
+[  "${DISABLE_JDK8}" != "1" ] && /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND  $JDK8_ARGS_APPEND" $JDK_JMH_EXTRA  $JDK8_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk8.json  | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk8.log.txt
+[ "${DISABLE_JDK11}" != "1" ] && /usr/lib/jvm/java-11-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK11_ARGS_APPEND" $JDK_JMH_EXTRA $JDK11_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk11.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk11.log.txt
+[ "${DISABLE_JDK17}" != "1" ] && /usr/lib/jvm/java-17-openjdk-amd64/bin/java    -jar "$JAR_FILE" -jvmArgsAppend "$JDK_ARGS_APPEND $JDK17_ARGS_APPEND" $JDK_JMH_EXTRA $JDK17_JMH_EXTRA -rf json -rff "$DATA_DIR"/jdk17.json | stdbuf -oL sed "s#${SITE_DIR}##g" | tee "$DATA_DIR"/jdk17.log.txt
